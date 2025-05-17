3425a4a42fce3b6cf44e605090131421343c4d7b
Roman <krill.kron@yandex.ru>
2025-05-17 13:15:20 +0300

    First commit.

---
+++ app/src/main/java/com/pobezhkin/loadercalculator/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.pobezhkin.loadercalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.pobezhkin.loadercalculator.ui.theme.LoaderCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState : Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LoaderCalculatorTheme {
+              /*  Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }*/
+            }
+        }
+    }
+}
+
+
+
