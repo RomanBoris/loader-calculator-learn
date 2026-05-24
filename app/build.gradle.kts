b394ba032d94684f11ee3327ca61b0902c04835b
Roman <Krill.kron@yandex.ru>
2026-05-24 17:21:48 +0300

    add dependencies and directory navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ android {
 
 dependencies {
 
+    implementation("androidx.navigation:navigation-compose:2.8.9")
     implementation ("androidx.hilt:hilt-navigation-compose:1.2.0")
     implementation("com.google.dagger:hilt-android:2.56.2")
     kapt("com.google.dagger:hilt-android-compiler:2.56.2")
